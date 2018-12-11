87d23d718bd1b78b03252b28b694e3ae87d6affa
Simon Gokstorp <simon@gokstorp.se>
2018-12-11 18:03:23 +0000

    Add structure of loops for optimised_gemm

--- section3/optimised-gemm.c
+++ section3/optimised-gemm.c
@@ -17,6 +17,34 @@ void optimised_gemm(int m, int n, int k,
                     const double *b, int ldb,
                     double *c, int ldc)
 {
-    /* Default implementation just calls the basic routine. */
-    return basic_gemm(m, n, k, a, lda, b, ldb, c, ldc);
+    // Split A into columns k_c wide. Access these simultaneously as the i'th index of column/row
+    for(int loop_1 = 0; loop_1 < k; loop_1 += k_c) {
+        // Pack the row from B
+        //TODO pack B
+        // Split the column from A into blocks m_c tall
+        for(int loop_2 = 0; loop_2 < k; loop_2 += m_c) {
+            // Pack the block from A
+            //TODO pack A
+            // Split the row from B into columns n_r wide
+            for(int loop_3 = 0; loop_3 < n; n_r) {
+                // Extract this column into a new data structure
+
+                // Split the block from the column from A into rows m_r tall
+                for(int loop_4 = 0; loop_4 < m_c; loop_4 += m_r) {
+                    // Extract this row into a new data structure
+
+                    // Multiply the row from A with the column from B store it in temp_output
+
+                    // Loop over the columns of temp_output
+                    for(int loop_5 = 0; loop_5 < n_r; loop_5++) {
+                        // Loop over each value in these columns
+                        for(int loop_6 = 0; loop_6 < m_r; loop_6++) {
+                            // Move this value into the correct index of C
+                            //TODO insert into C
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
