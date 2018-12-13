9336fdcf47582815b46c23e3be3156984f63cc0b
Simon Gokstorp <simon@gokstorp.se>
2018-12-13 13:57:48 +0000

    Change parameters for benchmarking

--- section3/optimised-gemm.c
+++ section3/optimised-gemm.c
@@ -9,22 +9,22 @@ void pack_b(int start, const int n, const double *b, const int ldb);
 void micro_kernel(int loop_2, int loop_3, int loop_4, int ldc, double *c);
 
 // Original values
-// const int m_r = 4;
+const int m_r = 4;
+const int n_r = 8;
+const int k_c = 256;
+const int m_c = 512;
+
+// Experimental values
+// const int m_r = 64;
 // const int n_r = 8;
 // const int k_c = 256;
 // const int m_c = 512;
 
-// Experimental values
-// int m_r = 64;
-// int n_r = 8;
-// int k_c = 256;
-// int m_c = 512;
-
 // Experimental 2 values - from parameter testing results
-int m_r = 128;
-int n_r = 8;
-int k_c = 128;
-int m_c = 512;
+// const int m_r = 128;
+// const int n_r = 8;
+// const int k_c = 128;
+// const int m_c = 512;
 
 
 // Global variable to store the packed subarray of A
